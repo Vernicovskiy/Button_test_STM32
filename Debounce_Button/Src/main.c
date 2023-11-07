3df9870b010f980b55ef36e55051e3364f100cc1
Vernicovskiy <victor02vernicovskiy@gmail.com>
2023-11-07 12:33:39 +0300

    Added DWT tick calculating in First commit(main) project version

--- Debounce_Button/Src/main.c
+++ Debounce_Button/Src/main.c
@@ -4,12 +4,6 @@
 #define LED_OFF() GPIOA->BSRR = GPIO_BSRR_BR5;
 
 
-	//uint8_t res = 0;
-	uint32_t count = 0;
-	uint32_t ref_count = 3;
-	uint32_t state = 0;
-
-
 
 	void gpio_init(){
 
@@ -22,49 +16,18 @@
 
 	}
 
-	uint8_t Debounce_Handler(uint32_t state){
-
-		if (state == 0){
-
-			if (count < ref_count){
-				count++;
-				return 0;
-			}
-
-			else {
-					return 1;		// button pressed
-				}
-			}
-
-		else {
-				if (count > 0){
-					count--;
-					return 1;
-				}
-
-				else {
-
-					if (state != 0){
-						return 0;
-					}
-				}
-
-		}
-
-		}
-
-
 
 	int main(void){
 
 		gpio_init();
+		DWT->CTRL |= 1;
+
 
 
 		while(1){
-
-			state = READ_BIT(GPIOC->IDR, GPIO_IDR_ID13);
-
-			if (Debounce_Handler(state) == 1){
+			asm("NOP");
+			DWT->CYCCNT = 0;
+			if (!READ_BIT(GPIOC->IDR, GPIO_IDR_ID13)){
 				LED_ON();
 			}
 			else {
